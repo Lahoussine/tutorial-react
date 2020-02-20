3d47eb60b049b5a683ee7e75160dd6f992612d48
lahoussine.derradji@gmail.com <lahoussine.derradji@gmail.com>
2020-02-20 09:46:00 +0100

    Updates

---
+++ precache-manifest.7f13a38c0c56e5b1bc8fb44fa75e2a39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5161e91e476d16e4ae22d397fc3b540",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "5967925f239d242f3eff",
+    "url": "/tutorial-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "dfe4ceca2c9a299286b2",
+    "url": "/tutorial-react/static/css/main.cfed1acd.chunk.css"
+  },
+  {
+    "revision": "5967925f239d242f3eff",
+    "url": "/tutorial-react/static/js/2.70f9ad9c.chunk.js"
+  },
+  {
+    "revision": "7c03a0c34b766d1b6b8534dd04e34f76",
+    "url": "/tutorial-react/static/js/2.70f9ad9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfe4ceca2c9a299286b2",
+    "url": "/tutorial-react/static/js/main.d351809d.chunk.js"
+  },
+  {
+    "revision": "1ce943025999b60b0983",
+    "url": "/tutorial-react/static/js/runtime-main.f93982bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tutorial-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
